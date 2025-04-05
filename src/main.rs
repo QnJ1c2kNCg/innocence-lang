67c47a53644053907b2a7f791ef31ab88c27ba4b
Bruno Roy <brunoroy1024@gmail.com>
2025-04-05 22:53:58 +0545

    Basic entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{env, process::exit};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => run_prompt(),
+        2 => run_file(&args[1]),
+        _ => {
+            eprintln!("Usage: innocence [script]");
+            exit(64);
+        }
+    }
+}
+
+fn run_file(args: &String) {
+    todo!()
+}
+
+fn run_prompt() {
+    todo!()
 }
