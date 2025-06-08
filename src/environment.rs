2ce9da6baf343b3854076c85e685fe10a4a48074
Bruno Roy <brunoroy1024@gmail.com>
2025-06-07 23:34:56 -0400

    [Environment] Add unit tests

--- src/environment.rs
+++ src/environment.rs
@@ -6,6 +6,7 @@ use std::{
 
 use crate::{interpreter::Value, tokens::Identifier};
 
+#[derive(Debug)]
 pub(crate) enum EnvironmentError {
     UndefinedVariable(Identifier),
 }
@@ -65,12 +66,66 @@ impl Environment {
     /// Get the value assigned to an [`Identifier`], if it is defined.
     pub(crate) fn get(&self, id: &Identifier) -> Option<Value> {
         match self.values.borrow().get(id) {
-            // TODO: not a fan of the clone here, should return a ref
+            // TODO: not a fan of the clone here, should return a Ref
             Some(value) => Some(value.clone()),
-            None => self
-                .parent
-                .as_ref()
-                .and_then(|enclosing| enclosing.get(id)),
+            None => self.parent.as_ref().and_then(|enclosing| enclosing.get(id)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn define_assign_get_happy_case() {
+        let env = Environment::new_global();
+        let id = Identifier::new("foo".to_owned());
+
+        assert!(env.get(&id).is_none());
+        env.define(id.clone(), Value::Number(42f64));
+        assert!(matches!(env.get(&id).unwrap(), Value::Number(42f64)));
+
+        env.assign(id.clone(), Value::Number(420f64)).unwrap();
+        assert!(matches!(env.get(&id).unwrap(), Value::Number(420f64)));
+    }
+
+    #[test]
+    fn outer_inner_env_lookup() {
+        let outer = Rc::new(Environment::new_global());
+        let id_outer = Identifier::new("outer".to_owned());
+        outer.define(id_outer.clone(), Value::Number(42f64));
+
+        let inner = Environment::new(outer.clone());
+        let id_inner = Identifier::new("inner".to_owned());
+        inner.define(id_inner.clone(), Value::Number(420f64));
+
+        // the inner environment has access to both variable
+        assert!(matches!(
+            inner.get(&id_outer).unwrap(),
+            Value::Number(42f64)
+        ));
+        assert!(matches!(
+            inner.get(&id_inner).unwrap(),
+            Value::Number(420f64)
+        ));
+
+        // the outer environment only has access to the outer variables
+        assert!(matches!(
+            outer.get(&id_outer).unwrap(),
+            Value::Number(42f64)
+        ));
+        assert!(matches!(outer.get(&id_inner), None));
+    }
+
+    #[test]
+    fn assign_undefined_variable() {
+        let env = Environment::new_global();
+        let id = Identifier::new("outer".to_owned());
+
+        // assigning a variable that was not defined
+        let err = env.assign(id.clone(), Value::Number(420f64)).unwrap_err();
+
+        assert!(matches!(err, EnvironmentError::UndefinedVariable(_)));
     }
 }
